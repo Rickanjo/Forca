c106cb041ef8a61ff43deafebdc427c5c81524c6
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-26 13:11:32 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("----Forca----\n");
+
+String PalavraSecreta;
+String Letra;
+Console.Write("Qual a palavra Secreta?");
+PalavraSecreta = Console.ReadLine()!;
+
+Console.Write("Qual é a letra?" );
+Letra = Console.ReadLine()!
+.Trim()
+.Substring(0,1)
+.ToLower();
+
+bool LetraExistente = PalavraSecreta
+.ToLower()
+.Contains (Letra);
+
+Console.WriteLine($"\nA letra \"{Letra}\" existe na palavra secreta => {LetraExistente}");
